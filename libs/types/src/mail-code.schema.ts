29c5beb36d22fb63deae6c91870510ebe236d821
Grant <284885069@qq.com>
2025-10-28 10:26:31 +0800

    feat: use zod 3.x

--- libs/types/src/mail-code.schema.ts
+++ libs/types/src/mail-code.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const MailActionEnum = z.enum(["register", "login", "reset-password"]);
 
 export const SendCodeSchema = z.object({
-  email: z.email({ message: "邮箱格式不正确" }).describe("邮箱"),
+  email: z.string().email({ message: "邮箱格式不正确" }).describe("邮箱"),
   action: MailActionEnum.describe("操作类型"),
 });
 
